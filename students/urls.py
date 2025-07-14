b8206a02c1d055c7071c8aab20bde9ac14083ef2
Shabna K S <shabna.ks@innovaturelabs.com>
2025-07-14 14:36:05 +0530

    feat: add CSV import functionality for creating students

--- students/urls.py
+++ students/urls.py
@@ -1,10 +1,13 @@
 from rest_framework.routers import DefaultRouter
 from .views import StudentViewSet
 from django.urls import path, include
+from .views import StudentCSVImportView
+
 
 router = DefaultRouter()
 router.register(r'students', StudentViewSet, basename='student')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('students/import-csv', StudentCSVImportView.as_view(), name='import-students-csv'),
 ]
